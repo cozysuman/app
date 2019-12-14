435e6e5a10dbfe94e3720aca28d763899986a048
Suman <sumanizrhythm@gmail.com>
2019-12-14 23:15:21 +0545

    Updates

---
+++ precache-manifest.6d9780a5e9f9d3d1ae1ec04b53aa2092.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8258a9270a6d75f63e318f04b279cb0",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "fda1a103c90659b0a62b",
+    "url": "/app/static/css/main.7ac8e65f.chunk.css"
+  },
+  {
+    "revision": "dcb5da346fa0b77320b0",
+    "url": "/app/static/js/2.2b545bda.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/app/static/js/2.2b545bda.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fda1a103c90659b0a62b",
+    "url": "/app/static/js/main.f590cfaa.chunk.js"
+  },
+  {
+    "revision": "dfb14fa9044e7d3e7e56",
+    "url": "/app/static/js/runtime-main.77b80642.js"
+  },
+  {
+    "revision": "0f0cb234b0e04db2b3bbc6233988ba6d",
+    "url": "/app/static/media/drizzle.0f0cb234.gif"
+  },
+  {
+    "revision": "aa895126d1033a2d751cd4928c69feda",
+    "url": "/app/static/media/fog.aa895126.gif"
+  },
+  {
+    "revision": "1bfccb37e12bf74bd56b607903f15ba7",
+    "url": "/app/static/media/rain.1bfccb37.gif"
+  },
+  {
+    "revision": "526c87ab14163f65cd6da978b1693eb5",
+    "url": "/app/static/media/snow.526c87ab.gif"
+  },
+  {
+    "revision": "78e15a7a1ba1e5497d967d7a9d6edf2b",
+    "url": "/app/static/media/sunny.78e15a7a.gif"
+  },
+  {
+    "revision": "425b811084dd2421c1df0fbe7576d883",
+    "url": "/app/static/media/thunderstrom.425b8110.gif"
+  },
+  {
+    "revision": "b1c5efa1117e6169675ffe920927a32b",
+    "url": "/app/static/media/weather.b1c5efa1.gif"
+  }
+]);
